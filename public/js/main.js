90cfb9308e1a476b62535fcf8a8bbdd954fef7cb
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-05-29 20:28:44 -0400

    add randomly generating pipes

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 var mainState = {
     preload: function() {
         game.load.image('bird', 'assets/bird.png');
+        game.load.image('pipe', 'assets/pipe.png');
     },
     create: function() {
         game.stage.backgroundColor = '#71c5cf';
@@ -10,6 +11,8 @@ var mainState = {
         this.bird.body.gravity.y = 1000;
         var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
+        this.pipes = game.add.group();
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
     },
     update: function() {
         if (this.bird.y < 0 || this.bird.y > 490){
@@ -18,6 +21,24 @@ var mainState = {
     },
     jump: function() {
         this.bird.body.velocity.y = -350;
+    },
+    addOnePipe: function(x, y) {
+        var pipe = game.add.sprite(x, y, 'pipe');
+        this.pipes.add(pipe);
+        game.physics.arcade.enable(pipe);
+        pipe.body.velocity.x = -200;
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
+    },
+    addRowOfPipes: function() {
+        console.log('addRowOfPipes');
+        var hole = Math.floor(Math.random() * 5) + 1;
+        console.log('hole: ' + hole);
+        for (var i = 0; i < 8; i++) {
+            if (i !== hole && i !== hole + 1) {
+                this.addOnePipe(400, i * 60 + 10);
+            }
+        }
     },
     restartGame: function() {
         game.state.start('main');
